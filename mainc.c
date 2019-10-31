f2e774c1bff1c5c9a27e11cc871241f8789a1935
seyoungyook <xqd1219@naver.com>
2019-10-31 16:16:55 +0900

    practice6

--- mainc.c
+++ mainc.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define ROWS 3
+#define COLS 3
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
 	
-	int i;
-	int grade[5];
-	int score[5];
-	
-	for (i=0;i<5;i++)
-	{
-		grade[i] = rand()%100;
-	}
-	
-	for (i=0;i=5;i++)
-	{
-		score[i] = grade[i];
-	
-	}
-	for (i=0;i<5;i++)
-	{
-		printf("score[%d] = %d\n", i, score[i]);
-	}
+int A[ROWS][COLS] = {
+	{2,3,0},
+	{8,9,1},
+	{7,0,5}};
+int B[ROWS][COLS] = {
+	{1,0,0},
+	{0,1,0},
+	{0,0,1}};
+int C[ROWS][COLS];
+
+addMatrix(A,B,C);
+printMatrix(C);
+
 	return 0;
 }
 
